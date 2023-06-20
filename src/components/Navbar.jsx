f28184bcbae2c0908cd016350174c8d94e73b235
RISCode1010 <92925907+RISCode1010@users.noreply.github.com>
2023-06-20 22:27:58 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import {NavLink} from "react-router-dom";
+import photo from "./p3.png";
+
+export default function Navbar() {
+  return (
+    <div>
+      <header>
+        <div className="logo">
+        <img className="lo" src={photo} alt="" />
+          <div className="heading">
+            TODAY's <span>TOP</span>
+          </div>
+        </div>
+        <div className="menu">
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/business">Business</NavLink>
+          <NavLink to="/entertainment">Entertainment</NavLink>
+          <NavLink to="/health">Health</NavLink>
+          <NavLink to="/science">Science</NavLink>
+          <NavLink to="/sports">Sports</NavLink>
+          {/* <NavLink to="/tecnology">Technology</NavLink> */}
+        </div>
+      </header>
+    </div>
+  );
+}
