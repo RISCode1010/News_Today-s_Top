09e92ff59496f9fa321119e33b8366d4f1ba0059
RISCode1010 <92925907+RISCode1010@users.noreply.github.com>
2023-06-22 11:21:23 +0530

    6 commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,13 @@
-import React from "react";
+import React,{useState} from "react";
 import {NavLink} from "react-router-dom";
 import photo from "./p3.png";
 
 export default function Navbar() {
+  const [clicked, setClicked] = useState(false)
+
+  const handleClick = ()=>{
+    setClicked(!clicked)
+  }
   return (
     <div>
       <header>
@@ -12,15 +17,26 @@ export default function Navbar() {
             TODAY's <span>TOP</span>
           </div>
         </div>
-        <div className="menu">
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/business">Business</NavLink>
-          <NavLink to="/entertainment">Entertainment</NavLink>
-          <NavLink to="/health">Health</NavLink>
-          <NavLink to="/science">Science</NavLink>
-          <NavLink to="/sports">Sports</NavLink>
-          <NavLink to="/technology">Technology</NavLink>
+        <div className="menu" id={clicked ? "active" : "menu"}>
+          {/* <div className="menu_inner"> */}
+            <div className="menu-link"><NavLink to="/">Home</NavLink></div>
+            <div className="menu-link"><NavLink to="/business">Business</NavLink></div>
+            <div className="menu-link"><NavLink to="/entertainment">Entertainment</NavLink></div>
+            <div className="menu-link"><NavLink to="/health">Health</NavLink></div>
+            <div className="menu-link"><NavLink to="/science">Science</NavLink></div>
+            <div className="menu-link"><NavLink to="/sports">Sports</NavLink></div>
+            <div className="menu-link"><NavLink to="/technology">Technology</NavLink></div>
+            
+
+            
+          {/* </div> */}
         </div>
+        <div id="mobile" onClick={handleClick}>
+            <i
+              id="bar"
+              className={clicked ? "fas fa-times" : "fas fa-bars"}
+            ></i>
+          </div>
       </header>
     </div>
   );
